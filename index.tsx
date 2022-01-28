2a2c56caab5dc247356c101b1293856fd957707b
myuon <ioi.joi.koi.loi@gmail.com>
2022-01-29 02:44:28 +0900

    feat: cells

--- index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { App } from "./src/App";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<App />, document.getElementById("root"));
--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,102 @@
 import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { css } from "@emotion/react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [sheet, setSheet] = useState(() =>
+    Array.from({ length: 10 }, () =>
+      Array.from({ length: 5 }, () => `${Math.random()}`)
+    )
+  );
+  const [selected, setSelected] = useState({ rowIndex: 0, colIndex: 0 });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div
+      css={css`
+        max-width: 80vw;
+        max-height: 80vh;
+        overflow: scroll;
+      `}
+    >
+      <div
+        css={[
+          css`
+            overflow: hidden;
+            width: 150px;
+            height: 20px;
+            line-height: 20px;
+            font-size: 14px;
+            cursor: pointer;
+          `,
+          css`
+            border: 3px solid black;
+            background-color: transparent;
+            transform: translate(-3px, -3px);
+            z-index: 1;
+          `,
+          css`
+            position: absolute;
+            top: ${selected.rowIndex * 20}px;
+            left: ${selected.colIndex * 150}px;
+          `,
+        ]}
+      />
+      <div
+        css={css`
+          overflow: scroll;
+          position: relative;
+          height: 100vh;
+
+          & > div {
+            border-bottom: 1px solid #ccc;
+
+            & > div:first-of-type {
+              border-left: 1px solid #ccc;
+            }
+
+            & > div {
+              border-right: 1px solid #ccc;
+            }
+          }
+        `}
+      >
+        {sheet
+          .map((row, rowIndex) =>
+            row.map((cell, cellIndex) => (
+              <div
+                key={`${rowIndex}-${cellIndex}`}
+                css={[
+                  css`
+                    overflow: hidden;
+                    width: 150px;
+                    height: 20px;
+                    line-height: 20px;
+                    font-size: 14px;
+                    cursor: pointer;
+                  `,
+                  css`
+                    position: absolute;
+                    top: ${rowIndex * 20}px;
+                    left: ${cellIndex * 150}px;
+                  `,
+                ]}
+                onClick={() => {
+                  setSelected({
+                    rowIndex,
+                    colIndex: cellIndex,
+                  });
+                }}
+              >
+                {selected.rowIndex === rowIndex &&
+                selected.colIndex === cellIndex
+                  ? "SELECTED"
+                  : cell}
+              </div>
+            ))
+          )
+          .flat()}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
